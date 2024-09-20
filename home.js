9851ff1cabc90f026304a7ebeb0b267b7ed60a62
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-09-20 12:39:31 +0600

    added partially the add money event handler

--- home.js
+++ home.js
@@ -1,1 +1,21 @@
-console.log('hela');
+/**
+ * step-1 set event handler
+ * step- Prevent default behaviour
+ * step 2 get money to be added to the account
+ */
+
+
+// add money to the account
+document.getElementById('btn-add-money').addEventListener('click',(e)=>{
+    // prevent default behaviour
+    e.preventDefault();
+    // step 2
+    const addMoneyInput = document.getElementById('input-add-money').value;
+
+    // get the pin number
+    const pinNumber = document.getElementById('input-pin-number').value;
+
+    // step 3 validation
+    
+
+})

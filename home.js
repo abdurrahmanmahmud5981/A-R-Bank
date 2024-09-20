b57676c8b890e5f00acb148445d79a53f20b9840
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-09-20 12:52:20 +0600

    validation added

--- home.js
+++ home.js
@@ -16,6 +16,22 @@ document.getElementById('btn-add-money').addEventListener('click',(e)=>{
     const pinNumber = document.getElementById('input-pin-number').value;
 
     // step 3 validation
-    
+    if(pinNumber === '1234'){
+        console.log('you can add money');
+        // step -4 get the current balance
+        const currentBalance = document.getElementById('current-balance').innerText;
+
+        // step 5 add new balance with current balance
+        const newBalance = parseFloat(currentBalance) + parseFloat(addMoneyInput);
+
+        // step 6 update the balance
+        document.getElementById('current-balance').innerText = newBalance;
+
+        // step 7 clear the input field
+        document.getElementById('input-add-money').value = '';
+        document.getElementById('input-pin-number').value = '';
+    }else{
+        alert('failed to add money ! Please try again');
+    }
 
 })

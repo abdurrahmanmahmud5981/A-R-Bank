d102a8a9b28254d3e71710e3b3cdabd8bf08bd34
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-09-20 12:06:04 +0600

    home page adde

--- script.js
+++ script.js
@@ -7,8 +7,19 @@
  * 
  */
 document.getElementById('btn-login').addEventListener('click', (e) => {
+    // step 2
     e.preventDefault()
-    
+    // step -3 phone and pin number
     const phone =  document.getElementById('phone-number').value;
-    console.log(phone);
+    const pin =  document.getElementById('pin-number').value;
+
+    // step 4 validation
+    // this is temporary .
+    if(phone === '5' && pin === '1234'){
+        console.log('You are logged in ');
+        window.location.href = './home.html';
+        // step 5 => allow user to access
+    }else{
+        alert('wrong phone or pin ');
+    }
 })
